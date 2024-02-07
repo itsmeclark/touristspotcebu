ff320556defd41c66c16842b3ec5da578f6ed0ec
itsmeclark <76141600+itsmeclark@users.noreply.github.com>
2024-02-07 09:53:34 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+function goOtherPage(input){
+    window.open(input, "_self")
+}
+function clickBait(){
+    window.open("rick.mp4","hJHSDJKSD", "width=350,height=250");
+    window.setTimeout(() => {
+        window.open("chi.mp4","asdsadsad", "width=350,height=250,top=10000,left=0");
+    }, 1000)
+    window.setTimeout(() => {
+        window.open("ruru.mp4","jdsfk", "width=350,height=250,top=10000,left=400");
+    }, 2000)
+    window.setTimeout(() => {
+        window.open("rit.mp4","erwrr", "width=350,height=250,top=10000,left=5000");
+    }, 2500)
+    window.setTimeout(() => {
+        window.open("www.mp4","wqewq", "width=350,height=250,top=0,left=5000");
+    }, 3500)
+    window.setTimeout(() => {
+        window.open("rus.mp4","wefg", "width=350,height=250,top=0,left=450");
+    }, 1500)
+    window.setTimeout(() => {
+        window.open("therock.mp4","bvcbc", "width=350,height=250,top=250,left=500");
+    }, 4000)
+}
